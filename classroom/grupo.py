1132a074d1d1334139311b044cbf6f4a1d383bea
SamuelCastaAlf <92555846+SamuelCastaAlf@users.noreply.github.com>
2023-09-05 00:07:39 -0500

    Prueba

--- classroom/grupo.py
+++ classroom/grupo.py
@@ -24,7 +24,7 @@ class Grupo:
         self.listadoAlumnos = self.listadoAlumnos + lista
 
     def __str__(self):
-        return "Grupo de estudiantes: "+ self._grupo
+        return ("Grupo de estudiantes: " + self._grupo)
 
     @ classmethod
     def asignarNombre(cls, nombre="Grado 10"):
@@ -32,9 +32,5 @@ class Grupo:
 
     @ classmethod
     def asignarNombre(cls, nombre="Grado 6"):
-        cls.grado = nombre
-
-    @ classmethod
-    def asignarNombre(cls, nombre="Grado 4"):
         cls.grado = nombre
 
